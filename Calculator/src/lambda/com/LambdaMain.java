ccce8ce6f1ec752f03880ce7f5c39dde79698386
Neelam <neelammujawar12@gmail.com>
2024-01-03 14:07:00 +0530

    Print result add , sub, div

--- Calculator/src/lambda/com/LambdaMain.java
+++ Calculator/src/lambda/com/LambdaMain.java
@@ -7,11 +7,11 @@ public static void main(String[] args) {
 System.out.println("Welcome to Calculator using Lambda");
 
 LambdaInterface sum = (int a,int b)->a+b;
-  
+System.out.println("Addition of a and  b is" +sum.Values(5, 4));
 LambdaInterface sub = (int a,int b)->a-b;
-   
+System.out.println("Substraction of a and  b is" +sub.Values(5, 4));
 LambdaInterface div = (int a,int b)->a / b;
-
+System.out.println("divsion  of a and  b is" +div .Values(5, 4));
 }
 
 	}
